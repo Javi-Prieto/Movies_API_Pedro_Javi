51e7cde68c59cf441a60e82e22fdfa9275be199e
Javier Prieto <jpojaviprior@gmail.com>
2023-11-09 11:52:11 +0100

    finished the branch and all in relation to it

--- movies_project/src/app/components/movie-item/movie-item.component.ts
+++ movies_project/src/app/components/movie-item/movie-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { PopularMovie } from 'src/app/models/movie-popular-list.interface';
+import { environment } from 'src/environments/environment.development';
 @Component({
   selector: 'app-movie-item',
   templateUrl: './movie-item.component.html',
   styleUrls: ['./movie-item.component.css']
 })
 export class MovieItemComponent {
+
+  @Input() movie!: PopularMovie;
+  setPosterUrl():string {
+    return `${environment.posterImageBaseUrl}${this.movie.poster_path}`;
+  }
 
 }
--- movies_project/src/app/ui/movie-list-page/movie-list-page.component.ts
+++ movies_project/src/app/ui/movie-list-page/movie-list-page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PopularMovie } from 'src/app/models/movie-popular-list.interface';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movie-list-page',
   templateUrl: './movie-list-page.component.html',
   styleUrls: ['./movie-list-page.component.css']
 })
-export class MovieListPageComponent {
-
+export class MovieListPageComponent{
+  
 }
