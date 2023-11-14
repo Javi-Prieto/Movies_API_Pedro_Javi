8fbaa4e4e1efe6a11c69dc091bf87339c1aae441
Javier Prieto <jpojaviprior@gmail.com>
2023-11-14 12:01:15 +0100

    Finished the searcher by letter and the pagination on the movie page

--- movies_project/src/app/components/movie-list/movie-list.component.ts
+++ movies_project/src/app/components/movie-list/movie-list.component.ts
@@ -9,8 +9,28 @@ import { MovieService } from 'src/app/services/movie.service';
 })
 export class MovieListComponent implements OnInit {
   movieList!: PopularMovie[];
+  numPage = 1;
+  numMovies = 0;
+
   constructor(private movieService:MovieService){}
   ngOnInit(): void {
-      this.movieService.getPopularMovieList().subscribe(resp => this.movieList = resp.results);
+      this.loadNewPage()
+  }
+  loadNewPage(){
+    this.movieService.getPopularMovieList(this.numPage).subscribe(resp => {
+      this.movieList = resp.results;
+      this.numMovies = resp.total_results;
+    });
+  }
+  loadPageByName(event:any){
+    let name: string = event.target.value; 
+    if(name == ''){
+      this.loadNewPage();
+    }else{
+      this.movieService.getMovieByName(event.target.value, this.numPage).subscribe(resp => {
+        this.movieList = resp.results;
+        this.numMovies = resp.total_results;
+      });
+    }
   }
 }
